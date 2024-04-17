c74eda90db9a998325553e05093449f0bd15c6a9
Guilherme Jacome De Freitas <gjacome-@c1r9s4.42lisboa.com>
2024-04-17 18:04:05 +0100

    small fix to structure of files

--- libft/srcs/strings/ft_atoi.c
+++ libft/srcs/strings/ft_atoi.c
@@ -6,11 +6,11 @@
 /*   By: gjacome- <gjacome-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 15:23:47 by gjacome-          #+#    #+#             */
-/*   Updated: 2024/04/17 17:51:00 by gjacome-         ###   ########.fr       */
+/*   Updated: 2024/04/17 18:02:56 by gjacome-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
+#include "../../libft.h"
 
 static int	ft_isspace(char c)
 {
--- libft/srcs/strings/ft_split.c
+++ libft/srcs/strings/ft_split.c
@@ -6,11 +6,11 @@
 /*   By: gjacome- <gjacome-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 15:16:44 by gjacome-          #+#    #+#             */
-/*   Updated: 2024/04/17 17:51:26 by gjacome-         ###   ########.fr       */
+/*   Updated: 2024/04/17 18:03:06 by gjacome-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
+#include "../../libft.h"
 
 static void	destroy(char ***arr)
 {
