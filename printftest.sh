8f61f1bdb334cfb1733491a45d1853e4ef9e2fcf
GJaFreitas <gui.jacome.freitas@gmail.com>
2024-04-18 19:07:02 +0000

    testing stuff

---
+++ printftest.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# Script to silence make since it always annoys me with a ton of text
+# Also cleaning the object files immediatly bc i dont like seeing them in vim file explorer
+
+cd ft_printf && make re >/dev/null || make
+make clean
+cd ..
+cc -g printftester.c ft_printf/libftprintf.a
+clear
+./a.out
---
+++ printftester.c
@@ -0,0 +1,32 @@
+#include "ft_printf/libftprintf.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	void	*ptr;
+	int	i = 42;
+	ptr = &i;
+	ft_printf("Testing Testing printf\n");
+	ft_printf("Void *:\n");
+	ft_printf("%p\n", ptr);
+	printf("Normal printf: %p\n", ptr);
+	ft_printf("Printing a %% without a specifier after:\n");
+	ft_printf("%\n");
+	ft_printf("Its eating the \\n \n");
+	printf("Normal printf: %\n");
+	ft_printf("Int, hex, char, unsigned max\n");
+	ft_printf("Int: %d\n", 42);
+	ft_printf("Hex: %x\n", 42);
+	ft_printf("Char: %c\n", 42);
+	ft_printf("Unsigned: %u\n", 4294967295);
+	printf("Normal printf:\n");
+	printf("Int: %d\n", 42);
+	printf("Hex: %x\n", 42);
+	printf("Char: %c\n", 42);
+	printf("Unsigned: %u\n", 4294967295);
+	ft_printf("Stringssss\n");
+	ft_printf("%s", "this whole thing is a string\n");
+	printf("Normal printf: %s", "this whole thing is a string\n");
+
+}
