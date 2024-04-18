59ff9094c74159594734bc1954d84dce90e16481
GJaFreitas <gui.jacome.freitas@gmail.com>
2024-04-18 13:27:43 +0000

    nice compile script

--- ft_printf/compile.sh
+++ ft_printf/compile.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
+
+# Script to silence make since it always annoys me with a ton of text
+# Also cleaning the object files immediatly bc i dont like seeing them in vim file explorer
+
 make >/dev/null || make
 make clean
