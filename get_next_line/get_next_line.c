39eb0c8afc655dd59e57406b8414bb118ebc8489
Guilherme Jacome De Freitas <gjacome-@c2r8s4.42lisboa.com>
2024-08-13 15:20:17 +0100

    done

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,26 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gjacome- <gjacome-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 15:40:53 by gjacome-          #+#    #+#             */
+/*   Updated: 2024/08/09 15:46:54 by gjacome-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char    *get_next_line(int fd)
+static	int	ft_while_condition(int *bytes_read, int fd, char *buff)
 {
-    static t_data *data;
-    t_data *temp;
-    char    *ret;
-    int     i;
+	*bytes_read = read(fd, buff, BUFFER_SIZE);
+	return (*bytes_read);
+}
 
-    i = 0;
-    if (data == NULL)
-    {
-        data = malloc(sizeof(t_data));
-        data->leftover = malloc(1);
-        *(data->leftover) = 0;
-        data->size = 1;
-    }
-    temp = data;
-    while (temp->leftover[i] && temp->leftover[i] != '\n')
-        i++;
-    if (temp->leftover[i] == '\n')
-        ret = ft_getline(temp, i + 1);
-    else
-        ret = read_line(temp, fd);
-    return (ret);
+char	*read_line(t_data *data, int fd)
+{
+	char	buff[BUFFER_SIZE + 1];
+	char	*ret;
+	int		bytes_read;
+	int		count;
+
+	count = 0;
+	ret = NULL;
+	buff[0] = 0;
+	while (ft_while_condition(&bytes_read, fd, buff) != 0)
+	{
+		if (bytes_read == -1)
+			return (NULL);
+		buff[bytes_read] = 0;
+		data->size += bytes_read;
+		ft_strjoin(data, buff);
+		while (data->leftover[count] && data->leftover[count++] != '\n')
+			;
+		if (data->leftover[count - 1] == '\n')
+			break ;
+	}
+	if (data->leftover[0] != 0)
+		ret = ft_getline(data, count);
+	return (ret);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_data	*data;
+	t_data			*temp;
+	char			*ret;
+	int				i;
+
+	i = 0;
+	if (data == NULL)
+	{
+		data = malloc(sizeof(t_data));
+		data->leftover = malloc(1);
+		*(data->leftover) = 0;
+		data->size = 1;
+	}
+	temp = data;
+	while (temp->leftover[i] && temp->leftover[i] != '\n')
+		i++;
+	if (temp->leftover[i] == '\n')
+		ret = ft_getline(temp, i + 1);
+	else
+		ret = read_line(temp, fd);
+	return (ret);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,51 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gjacome- <gjacome-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 15:41:09 by gjacome-          #+#    #+#             */
+/*   Updated: 2024/08/09 16:02:47 by gjacome-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-void    ft_strjoin(t_data *data, char const *s2)
+void	ft_strjoin(t_data *data, char const *s2)
 {
-    char    *new_str;
-    char    *temp;
-    int     i;
-    int     j;
+	char	*new_str;
+	char	*temp;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 0;
-    temp = data->leftover;
-    new_str = malloc(data->size + 1);
-    while (data->leftover[i])
-    {
-        new_str[i] = data->leftover[i];
-        i++;
-    }
-    while (i < data->size)
-        new_str[i++] = s2[j++];
-    new_str[i] = 0;
-    data->leftover = new_str;
-    free(temp);
+	i = 0;
+	j = 0;
+	temp = data->leftover;
+	new_str = malloc(data->size + 1);
+	while (data->leftover[i])
+	{
+		new_str[i] = data->leftover[i];
+		i++;
+	}
+	while (i < data->size)
+		new_str[i++] = s2[j++];
+	new_str[i] = 0;
+	data->leftover = new_str;
+	free(temp);
 }
 
-void    ft_parsestr(t_data *data, unsigned int size)
+void	ft_parsestr(t_data *data, unsigned int size)
 {
-    char    *new_str;
-    int     i;
-    int     j;
+	char	*new_str;
+	int		i;
+	int		j;
 
-    i = 0;
-    j = 0;
-    new_str = malloc(size + 1);
-    while (data->leftover[i] && data->leftover[i] != '\n')
-        i++;
-    if (data->leftover[i] == '\n')
-        i++;
-    while (data->leftover[i])
-    {
-        new_str[j] = data->leftover[i];
-        i++;
-        j++;
-    }
-    new_str[j] = 0;
-    free(data->leftover);
-    data->leftover = new_str;
-    data->size = size;
+	i = 0;
+	j = 0;
+	new_str = malloc(size + 1);
+	while (data->leftover[i] && data->leftover[i] != '\n')
+		i++;
+	if (data->leftover[i] == '\n')
+		i++;
+	while (data->leftover[i])
+	{
+		new_str[j] = data->leftover[i];
+		i++;
+		j++;
+	}
+	new_str[j] = 0;
+	free(data->leftover);
+	data->leftover = new_str;
+	data->size = size;
 }
 
 // TODO
@@ -55,74 +67,48 @@ void    ft_parsestr(t_data *data, unsigned int size)
 // as EOF, since there is no \n in the end and the file as been
 // read already. I need to find a way to go around this
 // Perhaps on the ft_getline function?
-char    *ft_edge_case(t_data *data, int flag)
+char	*ft_edge_case(t_data *data, int flag)
 {
-    char    *ret;
+	char	*ret;
 
-    ret = NULL;
-    if (flag == '\n')
-    {
-        ret = malloc(2);
-        ret[0] = '\n';
-        ret[1] = 0;
-        ft_parsestr(data, data->size - 0);
-    }
-    else if (flag == 0)
-    {
-        free(data->leftover);
-        free(data);
-    }
-    else
-    {
-        flag = 0;
-        while (data->leftover[flag])
-            flag++;
-        ret = ft_getline(data, flag);
-    }
-    return (ret);
+	ret = NULL;
+	if (flag == '\n')
+	{
+		ret = malloc(2);
+		ret[0] = '\n';
+		ret[1] = 0;
+		ft_parsestr(data, data->size - 0);
+	}
+	else if (flag == 0)
+	{
+		free(data->leftover);
+		free(data);
+	}
+	else
+	{
+		flag = 0;
+		while (data->leftover[flag])
+			flag++;
+		ret = ft_getline(data, flag);
+	}
+	return (ret);
 }
 
 char	*ft_getline(t_data *data, int count)
 {
-    char	*ret;
-    int	    i;
+	char	*ret;
+	int		i;
 
-    if (count == 0)
-        return (ft_edge_case(data, (int)data->leftover[0]));
-    i = 0;
-    ret = malloc(count + 1);
-    while (i < count)
-    {
-        ret[i] = data->leftover[i];
-        i++;
-    }
-    ret[i] = 0;
-    ft_parsestr(data, data->size - count);
-    return (ret);
-}
-
-char    *read_line(t_data *data, int fd)
-{
-    char    buff[BUFFER_SIZE + 1];
-    char    *ret;
-    int     bytes_read;
-    int     count;
-
-    count = 0;
-    ret = NULL;
-    buff[0] = 0;
-    while ((bytes_read = read(fd, buff, BUFFER_SIZE)) != 0)
-    {
-        if (bytes_read == -1)
-            return (NULL);
-        buff[bytes_read] = 0;
-        data->size += bytes_read;
-        ft_strjoin(data, buff);
-        while (data->leftover[count] && data->leftover[count++] != '\n');
-        if (data->leftover[count - 1] == '\n')
-            break ;
-    }
-    if (data->leftover[0] != 0)
-        ret = ft_getline(data, count);
-    return (ret);
+	if (count == 0)
+		return (ft_edge_case(data, (int)data->leftover[0]));
+	i = 0;
+	ret = malloc(count + 1);
+	while (i < count)
+	{
+		ret[i] = data->leftover[i];
+		i++;
+	}
+	ret[i] = 0;
+	ft_parsestr(data, data->size - count);
+	return (ret);
 }
