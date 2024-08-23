1e57820a4046a1284deacd93eab0f6df1cfdb38f
GJaFreitas <gui.jacome.freitas@gmail.com>
2024-08-23 17:19:59 +0100

    adding printf and get_next_line to libft

---
+++ libft/get_next_line/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gjacome- <gjacome-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/13 16:07:33 by gjacome-          #+#    #+#             */
+/*   Updated: 2024/08/16 19:51:54 by gjacome-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 50
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *str, char *buffer);
+int		ft_strlen(char *s, int flag);
+
+#endif
