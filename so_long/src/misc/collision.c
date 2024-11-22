2922da8c929e8acd18ecb23c73ae6ff38475c127
GJaFreitas <gui.jacome.freitas@gmail.com>
2024-11-22 11:03:15 +0000

    Addressing the collision allegations

--- so_long/src/misc/collision.c
+++ so_long/src/misc/collision.c
@@ -1,11 +1,16 @@
 #include "../../headers/header.h"
 
+// TODO: test this i cant rn
 // Return 1 when the positions collide and 0 when they dont
 int	collides(t_pos_vector pos1, t_pos_vector pos2)
 {
 	if (((pos1.x > pos2.x && pos1.x < pos2.x + pos2.w)\
 	&& (pos1.y > pos2.y && pos1.y < pos2.y + pos2.h))\
 	|| ((pos1.x + pos1.w > pos2.x && pos1.x + pos1.w < pos2.x + pos2.w)\
+	&& (pos1.y + pos1.h > pos2.y && pos1.y + pos1.h < pos2.y + pos2.h))
+	|| ((pos1.x + pos1.w > pos2.x && pos1.x + pos1.w < pos2.x + pos2.w)\
+	&& (pos1.y > pos2.y && pos1.y < pos2.y + pos2.h))
+	|| ((pos1.x > pos2.x && pos1.x < pos2.x + pos2.w)\
 	&& (pos1.y + pos1.h > pos2.y && pos1.y + pos1.h < pos2.y + pos2.h)))
 		return (1);
 	return (0);
